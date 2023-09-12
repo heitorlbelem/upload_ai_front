4fc60a12c9990d7df1b318c41788b93b16264e92
Heitor Belém <heitorlbelem98@gmail.com>
2023-09-12 02:27:29 -0300

    Initial config for shadcn and tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Button } from "./components/ui/button"
+import { Separator } from "./components/ui/separator"
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <div className="p-6 flex items-center justify-between">
+        <h1 className="text-3xl">upload.ai</h1>
+        <div className="flex items-center gap-4">
+          <span>Feito com muito 💜 pela RocketSeat.</span>
+          <Separator orientation="vertical" />
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
